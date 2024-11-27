a7d111b595bf45d5d7fa9387fb4589c65452a8f3
saivignesh2001 <dsaivigneshnani@gmail.com>
2024-11-27 21:39:03 +0530

    Initial commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-slate-200 '>
+      <p className='text-center font-bold'>Sri Goutham Krishna@2024</p>
+    </footer>
+  )
+}
+
+export default Footer
